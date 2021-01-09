3e5ff5efe5c865b3c55a8807896d9475ae544b70
John Jago <johnjago@protonmail.com>
2021-01-09 12:51:23 -0600

    Try out some more things with functions

--- functions.v
+++ functions.v
@@ -1,6 +1,12 @@
 fn main() {
   println(add(33, 50))
   println(sub(50, 342))
+  a, b := foo()
+  println(a)
+  println(b)
+  _, _ := foo()
+  println(foo())
+  println(concat("one", "two"))
 }
 
 fn add(x int, y int) int {
@@ -9,4 +15,16 @@ fn add(x int, y int) int {
 
 fn sub(x int, y int) int {
   return x - y
+}
+
+fn foo() (int, int) {
+  return 6, 7
+}
+
+fn concat(a ...string) string {
+  mut b := ""
+  for s in a {
+    b += s
+  }
+  return b
 }
