d1cd3634101f915b031c88b9e6ac5bd48fc54e4a
John Jago <johnjago@protonmail.com>
2021-01-11 21:15:43 -0600

    Primitive types

---
+++ types.v
@@ -0,0 +1,14 @@
+// Primitive types
+
+active := false
+println(typeof (active)) // bool
+
+title := "The Quick Brown Fox"
+println(typeof (title)) // string
+
+// Notice that I can name the variable "int"
+int := 53434
+println(typeof(int))
+println(typeof(i64(int))) // also i8, i16, int, etc.
+
+// Strings
